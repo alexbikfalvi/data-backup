d1f6a598c310d7ef96b4c7fda9b9ef4dee9130e0
Alex Bikfalvi <alex@bikfalvi.com>
2018-01-06 19:22:43 +0100

    Commit project skeleton
    This includes the Xcode project skeleton and main file.
    
    Signed-off-by: Alex Bikfalvi <alex@bikfalvi.com>

---
+++ DataBackup/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataBackup
+//
+//  Created by Alex Bikfalvi on 1/6/18.
+//  Copyright © 2018 Alex Bikfalvi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  std::cout << "Data Backup - Copyright (c) 2018 Alex Bikfalvi\n";
+  return 0;
+}
